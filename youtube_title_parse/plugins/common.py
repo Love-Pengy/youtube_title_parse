d7e489a0b8e65e82acc9c46a0b5ce1e7ef6e7c11
Love-Pengy <afrazierboy2001@gmail.com>
2025-05-30 18:40:27 -0400

    fix feat

--- youtube_title_parse/plugins/common.py
+++ youtube_title_parse/plugins/common.py
@@ -18,13 +18,11 @@ def clean_common_fluff(title):
     )
     title = re.sub(r"\(\s*(with\s+)?lyrics\s*\)\s*", "", title, flags=re.IGNORECASE)
     title = re.sub(r"\s*\(\s*\)", "", title)  # ()
-    # (Visualizer)
+
     title = re.sub(r"\(Visualizer\)", "", title, flags=re.IGNORECASE)    
-    # (Official Audio)
+    title = re.sub(r"\(Official Visualizer\)", "", title, flags=re.IGNORECASE)    
     title = re.sub(r"\(Official Audio\)", "", title, flags=re.IGNORECASE)
-    # (Audio)
     title = re.sub(r"\(Audio\)", "", title, flags=re.IGNORECASE)
-    # [Official Music Video] 
     title = re.sub(r"\[Official Music Video\]", "", title, flags=re.IGNORECASE)
     title = re.sub(r"\(Official Music Video\)", "", title, flags=re.IGNORECASE)
     title = re.sub(r"\(Mini Version\)", "", title, flags=re.IGNORECASE)
@@ -36,8 +34,12 @@ def clean_common_fluff(title):
     if("ft." in title): 
         title = re.sub(r"\(ft. [A-Za-z0-9]+\)", "", title, flags=re.IGNORECASE)
         title = title.split("ft.", 1)[0]
+        title = title.split("FT.", 1)[0]
+        title = title.split("Ft.", 1)[0]
     elif("feat." in title):  
         title = re.sub(r"\(feat. [A-Za-z0-9]+\)", "", title, flags=re.IGNORECASE)
         title = title.split("feat.", 1)[0]
+        title = title.split("Feat.", 1)[0]
+        title = title.split("FEAT.", 1)[0]
     
     return title.strip()
