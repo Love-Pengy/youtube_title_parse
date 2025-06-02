59afab2ee0f97fb37a4ec45f0dbb025e3e6890b4
Love-Pengy <afrazierboy2001@gmail.com>
2025-06-01 23:17:30 -0400

    add spaces

--- youtube_title_parse/plugins/common.py
+++ youtube_title_parse/plugins/common.py
@@ -16,30 +16,33 @@ def clean_common_fluff(title):
     title = re.sub(
         r"(\s*[-~_/]\s*)?\b(with\s+)?lyrics\s*", "", title, flags=re.IGNORECASE
     )
-    title = re.sub(r"\(\s*(with\s+)?lyrics\s*\)\s*", "", title, flags=re.IGNORECASE)
+    title = re.sub(r"\(\s*(with\s+)?lyrics\s*\)\s*",
+                   "", title, flags=re.IGNORECASE)
     title = re.sub(r"\s*\(\s*\)", "", title)  # ()
 
-    title = re.sub(r"\(Visualizer\)", "", title, flags=re.IGNORECASE)    
-    title = re.sub(r"\(Official Visualizer\)", "", title, flags=re.IGNORECASE)    
+    title = re.sub(r"\(Visualizer\)", "", title, flags=re.IGNORECASE)
+    title = re.sub(r"\(Official Visualizer\)", "", title, flags=re.IGNORECASE)
     title = re.sub(r"\(Official Audio\)", "", title, flags=re.IGNORECASE)
     title = re.sub(r"\(Audio\)", "", title, flags=re.IGNORECASE)
     title = re.sub(r"\[Official Music Video\]", "", title, flags=re.IGNORECASE)
     title = re.sub(r"\(Official Music Video\)", "", title, flags=re.IGNORECASE)
     title = re.sub(r"\(Mini Version\)", "", title, flags=re.IGNORECASE)
-    title = re.sub(r"\(prod. [A-Za-z0-9]+\)", "", title, flags=re.IGNORECASE)
+    title = re.sub(r"\(prod. [A-Za-z0-9 ]+\)", "", title, flags=re.IGNORECASE)
     title = re.sub(r"\(VIP\)", "", title, flags=re.IGNORECASE)
-    title = re.sub(r"\(dir. [A-Za-z0-9]+\)", "", title, flags=re.IGNORECASE)
+    title = re.sub(r"\(dir. [A-Za-z0-9 ]+\)", "", title, flags=re.IGNORECASE)
     title = re.sub(r"\(Screensaver\)", "", title, flags=re.IGNORECASE)
-    
-    if("ft." in title): 
-        title = re.sub(r"\(ft. [A-Za-z0-9]+\)", "", title, flags=re.IGNORECASE)
+
+    if ("ft." in title):
+        title = re.sub(r"\(ft. [A-Za-z0-9 ]+\)", "",
+                       title, flags=re.IGNORECASE)
         title = title.split("ft.", 1)[0]
         title = title.split("FT.", 1)[0]
         title = title.split("Ft.", 1)[0]
-    elif("feat." in title):  
-        title = re.sub(r"\(feat. [A-Za-z0-9]+\)", "", title, flags=re.IGNORECASE)
+    elif ("feat." in title):
+        title = re.sub(r"\(feat. [A-Za-z0-9 ]+\)",
+                       "", title, flags=re.IGNORECASE)
         title = title.split("feat.", 1)[0]
         title = title.split("Feat.", 1)[0]
         title = title.split("FEAT.", 1)[0]
-    
+
     return title.strip()
